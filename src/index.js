d11ffc8c7b14123e68cb23608a08bd36776f395d
Kyle <kdadika@gmail.com>
2023-05-08 23:22:54 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import App from "./App";
+
+import rootReducer from "./reducers/anecdoteReducer";
+
+const store = configureStore({ reducer: rootReducer });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
